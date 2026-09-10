918a275404a6fe253137576409c29142acb7b93e
Zachary Taylor <zachtaylorstudios@gmail.com>
2026-09-10 16:11:39 -0700

    feat: Add ability to list files in a directory

--- code/randopaper.cpp
+++ code/randopaper.cpp
@@ -3,24 +3,31 @@
 #include <windows.h>
 #include <fileapi.h>
 
-const int buffer_size = 255;
+const int buffer_size = MAX_PATH;
 char read_buffer[buffer_size];
 
 int check_file(char *file_path);
 void set_file(char *file_path);
 void get_file(void);
+void list_files(char *dir_path);
 
 int main(int argc, char *argv[])
 {
     for (int i = 0; i < argc; i++)
     {
-        if (strcmp(argv[i], "-s") == 0 && check_file(argv[i + 1]))
+        if (strcmp(argv[i], "-s") == 0 && i + 1 < argc && check_file(argv[i + 1]))
         {
             set_file(argv[i + 1]);
+            i++;
         }
         else if (strcmp(argv[i], "-g") == 0)
         {
             get_file();
+        }
+        else if (strcmp(argv[i], "-l") == 0 && i + 1 < argc)
+        {
+            list_files(argv[i + 1]);
+            i++;
         }
     }
 }
@@ -52,5 +59,40 @@ void get_file()
 {
     printf("Get\n");
     SystemParametersInfoA(SPI_GETDESKWALLPAPER, buffer_size, read_buffer, 0);
-    printf("Wallpaper: %s", read_buffer);
+    printf("Wallpaper: %s\n", read_buffer);
+}
+
+void list_files(char *dir_path)
+{
+    WIN32_FIND_DATA find_data;
+    HANDLE file_handle = FindFirstFileA(dir_path, &find_data);
+    if (file_handle == INVALID_HANDLE_VALUE)
+    {
+        printf("Invalid path\n");
+        return;
+    }
+
+    if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+    {
+        printf("Path is not a directory\n");
+        return;
+    }
+
+    strcat(dir_path, "\\*");
+    file_handle = FindFirstFileA(dir_path, &find_data);
+    if (file_handle == INVALID_HANDLE_VALUE)
+    {
+        printf("Directory is empty.\n");
+        return;
+    }
+
+    printf("Getting files...\n");
+    do
+    {
+        if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+        {
+            printf("File: %s\n", find_data.cFileName);
+        }
+    } while (FindNextFile(file_handle, &find_data));
+    FindClose(file_handle);
 }

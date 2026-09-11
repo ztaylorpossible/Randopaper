9f795f6b58509cf6f522ee45e96c5b3b6051dbe6
Zachary Taylor <zachtaylorstudios@gmail.com>
2026-09-11 14:43:35 -0700

    feat: Validate files based on supported image extensions

--- code/randopaper.cpp
+++ code/randopaper.cpp
@@ -3,9 +3,16 @@
 #include <windows.h>
 #include <fileapi.h>
 
+char *valid_extensions[] = {
+    "jpg",  "jpeg", "bmp",  "dib",   "png",  "jfif",  "jpe",   "gif",
+    "tif",  "tiff", "wdp",  "heic",  "heif", "heics", "heifs", "hif",
+    "avci", "avcs", "avif", "avifs", "jxr",  "jxl",   "webp",
+};
+
 const int buffer_size = MAX_PATH;
 char read_buffer[buffer_size];
 
+int validate_extention(char *file_path);
 int check_file(char *file_path);
 void set_file(char *file_path);
 void get_file(void);
@@ -32,18 +39,58 @@ int main(int argc, char *argv[])
     }
 }
 
+int validate_extension(char *file_path)
+{
+    int guard = 0;
+    char *current = file_path;
+    char *extension = NULL;
+
+    while (*current != '\0' && guard < 1000)
+    {
+        if (*current == '.')
+        {
+            extension = current;
+        }
+        current++;
+        guard++;
+    }
+
+    if (extension == NULL)
+    {
+        return 0;
+    }
+    extension++;
+
+    size_t check_len = sizeof(valid_extensions) / sizeof(char *);
+    for (int i = 0; i < check_len; i++)
+    {
+        if (strcmp(extension, valid_extensions[i]) == 0)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int check_file(char *file_path)
 {
-    int result = 0;
     WIN32_FIND_DATA find_data;
     HANDLE file_handle = FindFirstFileA(file_path, &find_data);
-    if (file_handle != INVALID_HANDLE_VALUE)
+    if (file_handle == INVALID_HANDLE_VALUE)
     {
-        result = 1;
+        FindClose(file_handle);
+        return 0;
     }
-    FindClose(file_handle);
 
-    return result;
+    if (validate_extension(file_path))
+    {
+        FindClose(file_handle);
+        return 1;
+    }
+
+    FindClose(file_handle);
+    return 0;
 }
 
 void set_file(char *file_path)
@@ -60,6 +107,7 @@ void get_file()
     printf("Get\n");
     SystemParametersInfoA(SPI_GETDESKWALLPAPER, buffer_size, read_buffer, 0);
     printf("Wallpaper: %s\n", read_buffer);
+    validate_extension(read_buffer);
 }
 
 void list_files(char *dir_path)
@@ -89,7 +137,7 @@ void list_files(char *dir_path)
     printf("Getting files...\n");
     do
     {
-        if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+        if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0 && validate_extension(find_data.cFileName))
         {
             printf("File: %s\n", find_data.cFileName);
         }

69160b7a5c3be1171ea8fd1b32b303360e0f04a4
Zachary Taylor <zachtaylorstudios@gmail.com>
2026-09-11 17:50:19 -0700

    refactor: Move some things into a header file

--- code/randopaper.cpp
+++ code/randopaper.cpp
@@ -2,21 +2,7 @@
 #include <string.h>
 #include <windows.h>
 #include <fileapi.h>
-
-char *valid_extensions[] = {
-    "jpg",  "jpeg", "bmp",  "dib",   "png",  "jfif",  "jpe",   "gif",
-    "tif",  "tiff", "wdp",  "heic",  "heif", "heics", "heifs", "hif",
-    "avci", "avcs", "avif", "avifs", "jxr",  "jxl",   "webp",
-};
-
-const int buffer_size = MAX_PATH;
-char read_buffer[buffer_size];
-
-int validate_extention(char *file_path);
-int check_file(char *file_path);
-void set_file(char *file_path);
-void get_file(void);
-void list_files(char *dir_path);
+#include "randopaper.h"
 
 int main(int argc, char *argv[])
 {
